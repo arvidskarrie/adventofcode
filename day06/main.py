fe4b2c976fdb212678329a4481343d1ace645e85
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2020-12-06 12:35:22 +0100

    day 6 refined + launh json

--- day06/main.py
+++ day06/main.py
@@ -4,51 +4,23 @@ import re
 from collections import Counter
 import string
 
-def part_1():
-    group_answers = {}
-    number_of_answers = 0
-
-    with open("input.txt") as _file:
-    #with open("input_test.txt") as _file:
-        for line in _file:
-            if line == '\n':
-                #Save group
-                number_of_answers += len(group_answers)
-                group_answers = {}
-
-            for char in line.rstrip():
-                group_answers[char] = 1
-    number_of_answers += len(group_answers)
-
-    return number_of_answers
-            
-
 def part_2():
-    group_answers = 'EMPTY'
     number_of_answers = 0
 
-    with open("input.txt") as _file:
-        for line in _file:
+    #with open("input.txt") as file:
+    with open("input_test.txt") as file:
+        group_answers = file.readline().rstrip()
+        for line in file:
             if line == '\n':
-                #Save group
-                print(group_answers, len(group_answers))
                 number_of_answers += len(group_answers)
-                group_answers = 'EMPTY'
-                continue
-
-            if group_answers == 'EMPTY':
-                group_answers = line.rstrip()
+                group_answers = file.readline().rstrip()
             else:
-                new_group_answers = ''
-                for char in line.rstrip():
-                    if char in group_answers:
-                        new_group_answers += char
-                group_answers = new_group_answers
+                group_answers = ''.join(set(group_answers).intersection(line.rstrip()))
 
-    print(group_answers, len(group_answers))
+    # process last line
     number_of_answers += len(group_answers)
 
     return number_of_answers
 
-print(part_1()) # 6443
+#print(part_1()) # 6443
 print(part_2()) # 3232
