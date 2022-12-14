fc744233ec38100597cc4820e9304a539938d255
Arvid Skarrie <arvid.skarrie@ctek.com>
2022-12-14 15:37:05 +0100

    day14

---
+++ 2022/day14a.py
@@ -0,0 +1,119 @@
+
+import os
+os.environ["AOC_SESSION"] = "53616c7465645f5feb5f98622da494e1f359f67b2973c8f6a54ed362910e50e251d3f40a7189ffd45624f53a2c2e408b0039c07d21c2423c1ebce73b8d6b4bce"
+
+from itertools import combinations
+import aocd
+import re
+
+
+USE_TEST_DATA = 0
+TEST_DATA = '498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9'
+
+if USE_TEST_DATA:
+    input_list = TEST_DATA.splitlines()
+else:
+    input_list = aocd.get_data().splitlines()
+
+START_TUPLE = tuple((500, 0))
+
+class Corner:
+    def __init__(self, word):
+        word = word.split(',')
+        xy_list = list(map(int, word))
+        self.x = xy_list[0]
+        self.y = xy_list[1]
+
+
+
+def add_rock_to_rock_set(rock, rock_coords):
+    for idx in range(len(rock)-1):
+        first_corner = rock[idx]
+        second_corner = rock[idx+1]
+
+        x_dir = 1 if first_corner.x <= second_corner.x else -1
+        y_dir = 1 if first_corner.y <= second_corner.y else -1
+        for x in range(first_corner.x, second_corner.x+x_dir, x_dir):
+            for y in range(first_corner.y, second_corner.y+y_dir, y_dir):
+                coord_tuple = (x, y)
+                rock_coords.add(coord_tuple)
+    return rock_coords
+    
+def print_coords(coords_1, coords_2):
+    for y in range(-2, 12) if USE_TEST_DATA else range(-2, 200):
+        rocks = ""
+        for x in range(490, 512) if USE_TEST_DATA else range(450, 520):
+            if (x, y) == (500, 0):
+                rocks += '+'
+            elif (x, y) in coords_1:
+                rocks += '#'
+            elif (x, y) in coords_2:
+                rocks += 'o'
+            else:
+                rocks += '.'
+        print(rocks)
+
+def get_sand_rest_place(rock_coords, sand_coords, sand_position, maximum_y_value, part):
+    sand_x = sand_position[0]
+    sand_y = sand_position[1]
+
+    # If outside a lower bounds, return 0
+    if part == 1:
+        if sand_y > maximum_y_value: # TODO
+            return 0
+    if part == 2:
+        if sand_y + 1 == maximum_y_value:
+            return sand_position
+
+    obstacle_set = rock_coords.union(sand_coords)
+
+    # If position below is free, go to that and iterate
+    if not (sand_x, sand_y+1) in obstacle_set:
+        return get_sand_rest_place(rock_coords, sand_coords, (sand_x, sand_y+1), maximum_y_value, part)
+
+    # If below and left are free, go to that and iterate
+    if not (sand_x-1, sand_y+1) in obstacle_set:
+        return get_sand_rest_place(rock_coords, sand_coords, (sand_x-1, sand_y+1), maximum_y_value, part)
+
+    # If below and right are free, go to that and iterate
+    if not (sand_x+1, sand_y+1) in obstacle_set:
+        return get_sand_rest_place(rock_coords, sand_coords, (sand_x+1, sand_y+1), maximum_y_value, part)
+    
+    # else, it rests here
+    return sand_position
+
+    
+def part_1(part):
+    rock_corners = []
+    maximum_y_value = 0
+
+    for line in input_list:
+        line = line.split(' ')
+        rock = []
+        for word in line:
+            if word != '->':
+                corner = Corner(word)
+                rock.append(corner)
+                maximum_y_value = max(maximum_y_value, corner.y)
+        rock_corners.append(rock)
+        
+    maximum_y_value += 2 if part == 2 else 0
+
+    rock_coords = set()
+    for rock in rock_corners:
+        rock_coords = add_rock_to_rock_set(rock, rock_coords)
+    
+    print('max', maximum_y_value)
+
+    sand_coords = set()
+    while True:
+        new_sand_pos = get_sand_rest_place(rock_coords, sand_coords, START_TUPLE, maximum_y_value, part)
+        if new_sand_pos == 0:
+            print_coords(rock_coords, sand_coords)
+            break
+        sand_coords.add(new_sand_pos)
+        # print_coords(rock_coords, sand_coords)
+
+    return len(sand_coords)
+
+print(part_1(1))
