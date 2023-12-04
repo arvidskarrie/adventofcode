d19e7ea01b7745d4db5127ca33bf0068796f8541
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2023-12-04 13:37:37 +0100

    3b

--- 2023 - python/day03.py
+++ 2023 - python/day03.py
@@ -1,7 +1,6 @@
 
 import os
 os.environ["AOC_SESSION"] = "53616c7465645f5f9cd071425b0d21d57535630a0d32e44bb5c7fe32070a07aa9170e67bb82f54f3bb900290fd1cb879c94a1e1230f809a51bec010d7f706512"
-from itertools import combinations
 import aocd
 import re
 
@@ -10,30 +9,24 @@ USE_TEST_DATA = 0
 TEST_DATA = '467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..'
 NUMBER_REGEX = r'(\d+)'
 
-def get_neighbours(coordinate, max_length, max_width):
+NO_STAR_FOUND = (-1, -1)
+
+def get_neighbours(coordinate):
     x, y = coordinate
     neighbours = []
 
     for dx in [-1, 0, 1]:
         for dy in [-1, 0, 1]:
-            if dx == 0 and dy == 0:
-                continue  # Skip the original coordinate itself
-
-            nx, ny = x + dx, y + dy
-
-            # Check if the new coordinates are within the boundaries
-            if 0 <= nx < max_width and 0 <= ny < max_length:
-                neighbours.append((nx, ny))
-
+            neighbours.append((x + dx, y + dy))
     return neighbours
     
-def is_adjacent_to_symbol(start_idx, match_length, line_idx, input_list, input_length, input_width):
+def get_any_adjacent_star(start_idx, match_length, line_idx, input_list):
     for char_idx in range(start_idx, start_idx + match_length):
-        neighbours = get_neighbours((char_idx, line_idx), input_length, input_width)
+        neighbours = get_neighbours((char_idx, line_idx))
         for (x, y) in neighbours:
-            if (not input_list[y][x].isdigit()) and (input_list[y][x] != '.'):
-                return True
-    return False
+            if 0 <= y < len(input_list) and 0 <= x < len(input_list[y]) and input_list[y][x] == '*':
+                return (x, y)
+    return NO_STAR_FOUND
 
 def part_1():
     if USE_TEST_DATA:
@@ -41,18 +34,27 @@ def part_1():
     else:
         input_list = aocd.get_data(day=3).splitlines()
 
-    input_length = len(input_list)
-    input_width = len(input_list[0])
-
+    # This dict will store any known stars with the adjacent number as value
+    star_dict = {}
     part_sum = 0
+
     for (line_idx, line) in enumerate(input_list):
         matches = re.finditer(NUMBER_REGEX, line)
 
         for match in matches:
             start_idx = match.start()
             match_length = len(match.group())
-            if is_adjacent_to_symbol(start_idx, match_length, line_idx, input_list, input_length, input_width):
-                part_sum += int(match.group())
+            star_coord = get_any_adjacent_star(start_idx, match_length, line_idx, input_list)
+
+            if NO_STAR_FOUND != star_coord:
+                if star_coord in star_dict.keys():
+                    # If the star is already in the dict, add the product between new and old number to sum
+                    # The old number is removed, even if it could be left as well.
+                    previous_part = star_dict.pop(star_coord)
+                    part_sum += int(match.group()) * previous_part
+                else:
+                    # Otherwise store it in the dictionary
+                    star_dict[star_coord] = int(match.group())
     
     print(part_sum)
 
