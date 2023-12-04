4a060dc837f78c4c7e32d63755762a4d9dfd4da3
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2023-12-04 11:30:27 +0100

    3a

--- 2023 - python/day03.py
+++ 2023 - python/day03.py
@@ -6,64 +6,55 @@ import aocd
 import re
 
 
-USE_TEST_DATA = 1
+USE_TEST_DATA = 0
 TEST_DATA = '467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..'
 NUMBER_REGEX = r'(\d+)'
 
-def get_neighbours(z):
-    x, y = z.real, z.imag
-    return [
-        complex(x + 1, y),     # Right
-        complex(x - 1, y),     # Left
-        complex(x, y + 1),     # Up
-        complex(x, y - 1),     # Down
-        complex(x + 1, y + 1), # Up-right
-        complex(x - 1, y - 1), # Down-left
-        complex(x + 1, y - 1), # Down-right
-        complex(x - 1, y + 1)  # Up-left
-    ]
+def get_neighbours(coordinate, max_length, max_width):
+    x, y = coordinate
+    neighbours = []
 
-def any_neighbour_is_symbol(z, symbol_list):
-    for neigh in get_neighbours(z):
-        if neigh in symbol_list:
-            return True
-    return False
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue  # Skip the original coordinate itself
+
+            nx, ny = x + dx, y + dy
+
+            # Check if the new coordinates are within the boundaries
+            if 0 <= nx < max_width and 0 <= ny < max_length:
+                neighbours.append((nx, ny))
+
+    return neighbours
     
+def is_adjacent_to_symbol(start_idx, match_length, line_idx, input_list, input_length, input_width):
+    for char_idx in range(start_idx, start_idx + match_length):
+        neighbours = get_neighbours((char_idx, line_idx), input_length, input_width)
+        for (x, y) in neighbours:
+            if (not input_list[y][x].isdigit()) and (input_list[y][x] != '.'):
+                return True
+    return False
+
 def part_1():
     if USE_TEST_DATA:
         input_list = TEST_DATA.splitlines()
     else:
         input_list = aocd.get_data(day=3).splitlines()
 
-    part_dict = {}
-    symbol_list = []
+    input_length = len(input_list)
+    input_width = len(input_list[0])
 
-    input_list.reverse()
+    part_sum = 0
     for (line_idx, line) in enumerate(input_list):
-        matches = re.findall(NUMBER_REGEX, line)
+        matches = re.finditer(NUMBER_REGEX, line)
 
         for match in matches:
-            start_idx = line.find(match)
-            part_dict[int(match)] = []
-            for i in range(start_idx, start_idx + len(match)):
-                part_dict[int(match)].append(i + line_idx * 1j)
-        
-        for (c_idx, c) in enumerate(line):
-            if not c.isdigit() and c != '.':
-                symbol_list.append(c_idx + line_idx * 1j)
+            start_idx = match.start()
+            match_length = len(match.group())
+            if is_adjacent_to_symbol(start_idx, match_length, line_idx, input_list, input_length, input_width):
+                part_sum += int(match.group())
     
-
-    print(part_dict)
-    # For each part, add to sum if there are any neightbours that are symbols
-    part_sum = 0
-    for (part, part_coords) in part_dict.items():
-        for coord in part_coords:
-            print("Coord: {}: {}".format(coord, get_neighbours(coord)))
-            if any_neighbour_is_symbol(coord, symbol_list):
-                part_sum += part
-                break
-    
-    print(part_sum) # 307158 too low
+    print(part_sum)
 
 
 part_1()
