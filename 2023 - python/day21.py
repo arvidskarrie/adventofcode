255f5a935c3248d7be1dc692818193582e6148e7
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2024-01-07 11:25:17 +0100

    21a stolen

--- 2023 - python/day21.py
+++ 2023 - python/day21.py
@@ -9,6 +9,10 @@ import itertools
 import functools
 import time
 from collections import deque
+from numpy import polyfit
+
+# NO_OF_STEPS = 1000
+NO_OF_STEPS = 26501365
 
 def find_start_pos(input):
     for y, line in enumerate(input):
@@ -16,41 +20,90 @@ def find_start_pos(input):
             if char == "S":
                 return (x, y) # TODO clean
 
+def quadratic_equation_and_value(x_coords, y_coords, final_x):
+    """
+    Fits a quadratic equation to three points and evaluates the equation at a given x-coordinate.
+
+    :param x_coords: List of three x-coordinates.
+    :param y_coords: List of three y-coordinates.
+    :param final_x: The x-coordinate at which to evaluate the fitted quadratic equation.
+    :return: Coefficients of the quadratic equation and the function value at final_x.
+    """
+    # Fit a quadratic equation (2nd degree polynomial) to the points
+    coefficients = polyfit(x_coords, y_coords, 2)
+
+    # The quadratic equation is of the form ax^2 + bx + c
+    a, b, c = coefficients
+
+    # Evaluate the quadratic equation at final_x
+    next_x = x_coords[-1] + 262
+    next_y = a * next_x**2 + b * next_x + c
+    print("Next y guess: {}".format(next_y))
+
+    # Evaluate the quadratic equation at final_x
+    final_y = a * final_x**2 + b * final_x + c
+    print("{} x^2 + {} x + {}".format(a, b, c))
+    print("final y = {}".format(final_y))
+
+    # return coefficients, final_y
+
 def run_bfs(input):
 
     assert(len(input) == len(input[0])) # Assert square
-    side_length = len(input)
+    side_length = len(input) # 131
+    double_side_length = 2 * side_length
+    
+    def get_index(s):
+        start_steps_value = NO_OF_STEPS % double_side_length
+        return (s + double_side_length - start_steps_value - 1) // double_side_length
+
     
     start_col, start_row = find_start_pos(input)
 
     seen_set = {(start_col, start_row)}
-    node_queue = deque([(start_col, start_row, 64)])
-    no_of_even_squares = 0
+    node_queue = deque([(start_col, start_row, 0)])
+    no_of_even_squares = {}
+    x_values = []
+    y_values = []
 
     while node_queue:
         # BFS means choosing the "eldest" node, which will automatically have the shortest path.
-        col, line, remaining_steps = node_queue.popleft()
+        col, line, steps_taken = node_queue.popleft()
 
         # if it is an even number of steps remaining, it is a solution
-        no_of_even_squares += remaining_steps % 2 == 0
+        if steps_taken % 2 == 1:
+            index = get_index(steps_taken)
+            if not index in no_of_even_squares.keys():
+                print("Finishing entry: At {} steps: {}".format(
+                    steps_taken - 2,
+                    sum(no_of_even_squares.values())
+                ))
+                x_values.append(steps_taken - 2)
+                y_values.append(sum(no_of_even_squares.values()))
+                if len(x_values) >= 6:
+                    quadratic_equation_and_value(
+                        x_values[-6:],
+                        y_values[-6:],
+                        NO_OF_STEPS)
+                # print("New index {} at {}".format(index, steps_taken))
+                no_of_even_squares[index] = 1
+            else:
+                no_of_even_squares[index] += 1
 
         # If there are no steps remaining, we are done
-        if remaining_steps == 0: 
+
+        if steps_taken == NO_OF_STEPS: 
             continue
 
         # Go through all its neighbours
         neighbour_list = [(col, line+1), (col, line-1), (col+1, line), (col-1, line)]
         for (n_col, n_line) in neighbour_list:
-            # If out of bounds, skip
-            if n_line < 0 or n_col < 0 or n_line >= side_length or n_col >= side_length:
-                continue
-
             # If the neighbour is already visited, no need to add it again
             if (n_col, n_line) in seen_set:
                 continue
 
             # Do not walk through rocks
-            if input[n_line][n_col] == "#":
+            if input[n_line % side_length][n_col % side_length] == "#":
                 continue
 
             # This square is now seen, with one step less than before
@@ -58,9 +111,11 @@ def run_bfs(input):
             seen_set.add((n_col, n_line))
 
             # We also need to investigate the neighbours of this node
-            node_queue.append((n_col, n_line, remaining_steps - 1))
+            node_queue.append((n_col, n_line, steps_taken + 1))
 
-    return no_of_even_squares
+    print("Total number of even nodes {}".format(sum(no_of_even_squares.values())))
+
+    return sum(no_of_even_squares.values())
                 
 
 def part_1():
@@ -72,4 +127,6 @@ def part_1():
     
 
 
-print(part_1() == 3651)
+print(part_1())
+
+# 607334325965753 too high
