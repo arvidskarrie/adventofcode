0186b53ef0c748b1ad5d780626bd6e0f5e1a479d
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2024-01-08 08:39:04 +0100

    22b

--- 2023 - python/day22.py
+++ 2023 - python/day22.py
@@ -7,8 +7,9 @@ import aocd
 USE_TEST_DATA = 0
 TEST_DATA = '1,0,1~1,2,1\n0,0,2~2,0,2\n0,2,3~2,2,3\n0,0,4~0,2,4\n2,0,5~2,2,5\n0,1,6~2,1,6\n1,1,8~1,1,9'
 
-def let_bricks_fall(bricks, early_exit = False):
-    changes_made = False
+def let_bricks_fall(bricks, only_small_falls = False):
+    number_of_falls = 0
+
     bricks.sort(key=lambda b: b[0][2])
     all_brick_parts = set(
         part
@@ -18,31 +19,31 @@ def let_bricks_fall(bricks, early_exit = False):
 
     brick_idx = 0
     while brick_idx < len(bricks):
-        if not early_exit:
+        if not only_small_falls:
             print("Idx {} / {}".format(brick_idx, len(bricks)))
         brick = bricks[brick_idx]
         for (x, y, z) in brick:
             if z == 1:
+                # On the floor
                 break
             if not (x, y, z - 1) in brick and (x, y, z - 1) in all_brick_parts:
                 break
         else:
             # Nothing in the way
-            if early_exit:
-                return True
-
-            changes_made = True
             new_brick = [(x_n, y_n, z_n - 1) for (x_n, y_n, z_n) in brick]
             for part in brick:
                 all_brick_parts.remove(part)
             for part in new_brick:
                 all_brick_parts.add(part)
             bricks[brick_idx] = new_brick
-            
-            continue
+            if not only_small_falls:
+                continue
+            else:
+                number_of_falls += 1
+
         brick_idx += 1
         
-    return changes_made
+    return number_of_falls
 
 def part_1():
 
@@ -68,25 +69,19 @@ def part_1():
 
         bricks.append(brick)
     
-        
-    while let_bricks_fall(bricks):
-        pass
+    # Starting from the bottom, no need to pass through multiple times    
+    let_bricks_fall(bricks)
     
     # Try to remove every brick and see if anything reacts
     # If nothing reacts, it is desintegrationable
-    integrationable_bricks = 0
-    for brick_idx, brick in enumerate(bricks):
-        print("\tIdx {} / {}".format(brick_idx, len(bricks)))
-        test_bricks = bricks[:]
-        test_bricks.remove(brick)
-        if not let_bricks_fall(test_bricks, True):
-            integrationable_bricks += 1
-            print(integrationable_bricks)
+    fallen_bricks = 0
+    for brick in bricks:
+        bricks.remove(brick)
+        fallen_bricks += let_bricks_fall(bricks, True)
+        bricks.append(brick)
 
-    pass
-
-            
-
+    print(fallen_bricks)
+    print(fallen_bricks == 79144)
 
 
 part_1()
